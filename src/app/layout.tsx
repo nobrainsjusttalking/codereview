46a83acb9e0d152bd5a8f81ff655446c9e6a758b
niko <reowqwerwerweafkdjhsf@mail.ru>
2025-06-29 14:40:00 +0300

    Quick import fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import Footer from '@/components/Footer/Footer';
 import GradientBackground from '@/components/GradientBackground/GradientBackground';
 import Header from '@/components/Header/Header';
 import type { Metadata } from 'next';
+import React from 'react';
 import { displayFont, textFont } from './fonts';
 import './globals.css';
 
