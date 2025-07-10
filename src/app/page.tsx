e509bf646e713c2c88523ad1fb9c5c2e17898c62
niko <reowqwerwerweafkdjhsf@mail.ru>
2025-07-11 00:31:23 +0300

    refactoring: static data page layout fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 'use client';
 import CategoryButton from '@/components/CategoryButton/CategoryButton';
 import IndexCard from '@/components/IndexCard/IndexCard';
-import { indexCardData } from '@/components/IndexCard/IndexCardData';
 import { useCategory } from '@/context/CategoryContext';
 import Image from 'next/image';
 import './App.css';
@@ -38,21 +37,90 @@ export default function Page() {
       </div>
 
       <div className='index-cards'>
-        {
-          indexCardData.map(({ href, heading, image, imgWidth, imgHeight, text, additionalClass = ''}, idx) => {
-            return (
-              <IndexCard href={href} key={`${heading}-${idx}`} additionalClass={additionalClass + ' ' + ((href == '#') ? 'index-card-in-dev' : '')}>
-                <div className='index-card-data'>
-                  <Image src={image} alt='' aria-hidden='true' width={imgWidth} height={imgHeight} />
+        <IndexCard href='vacancies/'>
+          <h2 className='index-card-heading'>Вакансии / Стажировки</h2>
 
-                  <p className='index-card-text hide-mobile'>{text}</p>
-                </div>
+          <div className='index-card-data'>
+            <Image src='/IndexCard/briefcase.svg' alt='' aria-hidden='true' width={26} height={26} />
 
-                <h2 className='index-card-heading'>{heading}</h2>
-              </IndexCard>
-            );
-          })
-        }
+            <p className='index-card-text hide-mobile'>Актуальные junior-вакансии и стажировки с hh.ru, Habr Career, LinkedIn, Telegram и других платформ</p>
+          </div>
+        </IndexCard>
+
+        <IndexCard href='#' additionalClass='index-card-in-dev'>
+          <h2 className='index-card-heading'>Необходимые навыки</h2>
+
+          <div className='index-card-data'>
+            <Image src='/IndexCard/stack.svg' alt='' aria-hidden='true' width={26} height={26} />
+
+            <p className='index-card-text hide-mobile'>Популярные навыки и ключевые слова из вакансий — можно использовать для улучшения резюме</p>
+          </div>
+        </IndexCard>
+
+        <IndexCard href='#' additionalClass='index-card-in-dev light-gradient-bg'>
+          <h2 className='index-card-heading'>Тестовые задания</h2>
+
+          <div className='index-card-data'>
+            <Image src='/IndexCard/code.svg' alt='' aria-hidden='true' width={26} height={26} />
+
+            <p className='index-card-text hide-mobile'>Реальные задания с отборов на стажировки и junior-позиции — для практики и портфолио</p>
+          </div>
+        </IndexCard>
+
+        <IndexCard href='#' additionalClass='index-card-in-dev'>
+          <h2 className='index-card-heading'>Собеседования</h2>
+
+          <div className='index-card-data'>
+            <Image src='/IndexCard/microphone.svg' alt='' aria-hidden='true' width={26} height={26} />
+
+            <p className='index-card-text hide-mobile'>Реальные вопросы с интервью, сгруппированные по стеку и уровню сложности — с пояснениями и примерами ответов</p>
+          </div>
+        </IndexCard>
+
+        <IndexCard href='#' additionalClass='index-card-in-dev purplish-gradient-bg'>
+          <h2 className='index-card-heading'>IT-мероприятия</h2>
+
+          <div className='index-card-data'>
+            <Image src='/IndexCard/calendar.svg' alt='' aria-hidden='true' width={26} height={26} />
+
+            <p className='index-card-text hide-mobile'>Конференции, хакатоны, метапы и курсы — чтобы расширить кругозор, найти стажировку и завести связи</p>
+          </div>
+        </IndexCard>
+
+        <IndexCard href='#' additionalClass='index-card-in-dev'>
+          <h2 className='index-card-heading'>Пет-проекты</h2>
+
+          <div className='index-card-data'>
+            <Image src='/IndexCard/code-asterisk.svg' alt='' aria-hidden='true' width={26} height={26} />
+
+            <p className='index-card-text hide-mobile'>Проекты для портфолио, в том числе коммерческие. Отличный способ прокачать скиллы и показать опыт</p>
+          </div>
+        </IndexCard>
+
+        <IndexCard href='#' additionalClass='index-card-in-dev'>
+          <h2 className='index-card-heading'>Контакты рекрутеров</h2>
+
+          <div className='index-card-data'>
+            <Image src='/IndexCard/message.svg' alt='' aria-hidden='true' width={26} height={26} />
+
+            <p className='index-card-text hide-mobile'>Контакты рекрутеров и сотрудников, готовых рекомендовать в свою компанию. Можно найти того, кто поможет попасть на собеседование</p>
+          </div>
+        </IndexCard>
+
+        <IndexCard href='#' additionalClass='index-card-in-dev'>
+          <h2 className='index-card-heading'>ИИ инструменты</h2>
+
+          <div className='index-card-data'>
+            <Image src='/IndexCard/sparkles.svg' alt='' aria-hidden='true' width={26} height={26} />
+
+            <p className='index-card-text hide-mobile'>Подборка AI-тулзов для автоматизации, разработки и поиска работы — с описаниями и рейтингом полезности</p>
+          </div>
+        </IndexCard>
+
+
+
+
+
         
         <IndexCard href='#' additionalClass='index-card-big'>
           <div className='index-card-data'>
